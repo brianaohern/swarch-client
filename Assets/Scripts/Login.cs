35e759a46f099c3ef61b77ff493ea1f2f13caaa5
zarazha <bohern@uci.edu>
2014-04-23 09:27:00 -0700

    Login Screen
    Still not working

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class Login : MonoBehaviour {
-	private string u = "Username";
-	private string p = "Password";
+//	private string u = "Username";
+//	private string p = "Password";
 
 	private string usernameString = string.Empty;
 	private string passwordString = string.Empty;
@@ -30,8 +30,12 @@ public class Login : MonoBehaviour {
 		usernameString = GUI.TextField(new Rect (Screen.width / 3, 2 * Screen.height / 5, Screen.width / 3, Screen.height / 5), usernameString, 10);
 		passwordString = GUI.PasswordField(new Rect (Screen.width / 3, 2 * Screen.height / 3, Screen.width / 3, Screen.height / 5), passwordString, '*');
 
+		if (GUI.Button(new Rect (Screen.width / 2, 4 * Screen.height / 5, Screen.width / 8, Screen.height / 8), "Login")) 
+		{
+			Debug.Log("HI");
+		}
+
 		GUI.Label(new Rect(Screen.width/3, 35 * Screen.height/100, Screen.width/5, Screen.height/8), "Username");
-		GUI.Label(new Rect(Screen.width/3, 62 * Screen.height/100, Screen.width/8, Screen.height/8), "Password");                     
-		
+		GUI.Label(new Rect(Screen.width/3, 62 * Screen.height/100, Screen.width/8, Screen.height/8), "Password");
 	}
 }
