5896f97c4a19aa86eca46c4224501181519758dd
zarazha <bohern@uci.edu>
2014-04-23 10:41:27 -0700

    Milestone 2
    Completed Milestone 2; Includes functional login screen (that doesn’t
    error check) and switches to working game

--- Assets/Scripts/EatPellets.cs
+++ Assets/Scripts/EatPellets.cs
@@ -7,6 +7,7 @@ public class EatPellets : MonoBehaviour {
 		if (other.gameObject.tag == "Pellet") {
 			other.transform.position = new Vector3(Random.Range(-29,30),0,Random.Range(-14,15));
 			transform.localScale = new Vector3(transform.localScale.x + 2, transform.localScale.y, transform.localScale.z + 2);
+			Movement.speed = (4 * Movement.speed / 5);
 		}
 	}
 }
--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -7,12 +7,15 @@ public class Login : MonoBehaviour {
 
 	private string usernameString = string.Empty;
 	private string passwordString = string.Empty;
+	public GUIText username;
+	public static bool playing;
 
 	private Rect windowRect = new Rect(0, 0, Screen.width, Screen.height);
 
 	// Use this for initialization
 	void Start () {
-	
+		playing = false;
+		username.text = string.Empty;
 	}
 	
 	// Update is called once per frame
@@ -20,19 +23,24 @@ public class Login : MonoBehaviour {
 	
 	}
 
-	void OnGui()
+	void OnGUI()
 	{
-		GUI.Window (0, windowRect, windowFunction, "Login");
+		if (!playing) 
+		{
+			GUI.backgroundColor = new Color(0, 0, 0);
+			GUI.Window (0, windowRect, windowFunction, "Login");
+		}
 	}
 
 	void windowFunction(int windowID)
 	{
-		usernameString = GUI.TextField(new Rect (Screen.width / 3, 2 * Screen.height / 5, Screen.width / 3, Screen.height / 5), usernameString, 10);
-		passwordString = GUI.PasswordField(new Rect (Screen.width / 3, 2 * Screen.height / 3, Screen.width / 3, Screen.height / 5), passwordString, '*');
+		usernameString = GUI.TextField(new Rect (Screen.width / 3, 2 * Screen.height / 5, Screen.width / 3, Screen.height / 9), usernameString, 10);
+		passwordString = GUI.PasswordField(new Rect (Screen.width / 3, 2 * Screen.height / 3, Screen.width / 3, Screen.height / 9), passwordString, '*');
 
-		if (GUI.Button(new Rect (Screen.width / 2, 4 * Screen.height / 5, Screen.width / 8, Screen.height / 8), "Login")) 
+		if (GUI.Button(new Rect (Screen.width / 2, 4 * Screen.height / 5, Screen.width / 9, Screen.height / 12), "Login")) 
 		{
-			Debug.Log("HI");
+			username.text = ("Username: " + usernameString);
+			playing = true;
 		}
 
 		GUI.Label(new Rect(Screen.width/3, 35 * Screen.height/100, Screen.width/5, Screen.height/8), "Username");
