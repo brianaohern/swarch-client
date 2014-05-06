60f4aee6a07fe0e0b31ca4eb309ad3c6abccbab5
Cheddar2012 <cheddar1@outlook.com>
2014-05-05 20:47:24 -0700

    Added client-side network scripts. Everything should work once we have the server side working.

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -4,23 +4,22 @@ using System.Collections;
 public class Login : MonoBehaviour {
 //	private string u = "Username";
 //	private string p = "Password";
-
+	
+	private GameProcess process;
+	public GUIText username;
+	
+	public static bool playing;
 	private string usernameString = string.Empty;
 	private string passwordString = string.Empty;
-	public GUIText username;
-	public static bool playing;
 
 	private Rect windowRect = new Rect(0, 0, Screen.width, Screen.height);
 
 	// Use this for initialization
 	void Start () {
+		process = GameObject.Find("GameProcess").GetComponent<GameProcess>();
+		
 		playing = false;
 		username.text = string.Empty;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 
 	void OnGUI()
@@ -29,6 +28,14 @@ public class Login : MonoBehaviour {
 		{
 			GUI.backgroundColor = new Color(0, 0, 0);
 			GUI.Window (0, windowRect, windowFunction, "Login");
+		}
+		if (playing) 
+		{
+			if ( GUI.Button( new Rect( 100, 200, 100, 20), "Disconnect"))
+			{
+				process.returnSocket().Disconnect();
+				playing = false;
+			}
 		}
 	}
 
@@ -39,11 +46,40 @@ public class Login : MonoBehaviour {
 
 		if (GUI.Button(new Rect (Screen.width / 2, 4 * Screen.height / 5, Screen.width / 9, Screen.height / 12), "Login")) 
 		{
-			username.text = ("Username: " + usernameString);
-			playing = true;
+			username.text = "Signing in...";
+			if ( process.returnSocket().Connect() )
+			{
+				playing = true;
+				username.text = ("Username: " + usernameString);
+			}
+			else username.text = "Connect Failed";
 		}
 
 		GUI.Label(new Rect(Screen.width/3, 35 * Screen.height/100, Screen.width/5, Screen.height/8), "Username");
 		GUI.Label(new Rect(Screen.width/3, 62 * Screen.height/100, Screen.width/8, Screen.height/8), "Password");
+	}
+	
+	public void printGui ( string printStr )
+	{
+		int wordCount = 0 ;
+		string[] words = printStr.Split(' ');
+		
+		printStr = "";
+		
+		for ( int i = 0 ; i < words.Length ; ++ i )
+		{
+			if ( wordCount <= 4 )
+			{
+				printStr += words[i] + " " ;
+				wordCount ++ ;
+			}
+			else
+			{
+				printStr += words[i] + "\n" ;
+				wordCount = 0;
+				
+			}	
+		}
+		username.text = printStr ;
 	}
 }
